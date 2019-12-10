c733746e4c2e099c537fd0b1fcb34c435f7e96b7
Marcos <jkmarcosb@gmail.com>
2019-12-10 11:20:10 -0600

    adding i parking adapter and upgrade to c# 8

---
+++ proyectoLibrary/Interface/IParkingAdapter.cs
@@ -0,0 +1,14 @@
+﻿using proyectoLibrary.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectoLibrary.Interface
+{
+    interface IParkingAdapter
+    {
+        public List<Parking> GetPakingList();
+    }
+}
